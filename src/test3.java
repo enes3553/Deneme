7396b21792b4a8612a4c9271c3abef3fac560bd3
enes3553 <93152484+enes3553@users.noreply.github.com>
2022-03-10 16:02:14 -0500

    change

--- src/test3.java
+++ src/test3.java
@@ -1,5 +1,7 @@
 public class test3 {
     public static void main(String[] args) {
         System.out.println(";;");
+        System.out.println("true = " + true);
+        System.out.println("false = " + false);
     }
 }
