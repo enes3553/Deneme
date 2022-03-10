feb264a613a59fe9bf82eb597ceed2467d682b40
enes3553 <93152484+enes3553@users.noreply.github.com>
2022-03-10 16:13:21 -0500

    change3

--- src/newClassE.java
+++ src/newClassE.java
@@ -2,5 +2,15 @@ public class newClassE {
     public static void main(String[] args) {
         System.out.println("true = " + true);
         System.out.println("love");
+        System.out.println("love");
+        System.out.println("love");
+        System.out.println("love");
+        System.out.println("love");
+        System.out.println("love");
+        System.out.println("love");
+        System.out.println("love");
+        System.out.println("love");
+        System.out.println("love");
+
     }
 }
